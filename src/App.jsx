b1e4242a85b816c448347d9c051fb211f6b8ed35
Erick Lopez <lrerick20001430@gmail.com>
2024-11-06 20:50:22 -0600

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="container">
+      <div className="App-form">
+        <h1>Examen segundo parcial</h1>
+        <div>
+          <p>Contenido aquí</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
